54176aa1e0607ccf7e98e7b4bbf6eb29e96d2c60
Amukta Yajamanyam <amukta@Amuktas-MacBook-Air.local>
2025-05-22 00:58:53 +0530

    WIP: stage all changes before rebase

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['raw.githubusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/devicons/devicon/**',
-      }
+        hostname: '**',
+      },
     ],
   },
 }
